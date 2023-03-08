0f5e9c2999f660343bd951ead17d486c64ae9e5b
Tony Goat <70238376+Tony-Goat@users.noreply.github.com>
2023-03-07 23:07:57 -0700

    Byte ordering magic
    Rotated the image during processing to take advantage of image byte ordering to allow for a much quicker memcopy.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 use std::path::PathBuf;
 
-use image::imageops::resize;
 use image::imageops::FilterType;
+use image::imageops::{resize, rotate270};
 use image::io::Reader as ImageReader;
 use image::DynamicImage;
 use image::RgbImage;
@@ -35,12 +35,13 @@ fn main() {
     let old_height = src_imgs[0].height();
     let time_dim = src_imgs.len() as u32;
 
-    //Resize all of the images so they're as many pixels wide as there are frames
+    //Resize, rotate, and flip all of the images so they're as many pixels wide as there are frames
     let resized_imgs: Vec<RgbImage> = src_imgs
         .into_par_iter()
         .progress_count(time_dim as u64)
         .map(|i| {
             i.resize_exact(time_dim, old_height, FilterType::CatmullRom)
+                .rotate90()
                 .into_rgb8()
         })
         .collect();
@@ -54,16 +55,26 @@ fn main() {
     println!("{p} frames processed successfully.");
 }
 
+const BPP: u32 = 3;
+
 fn resize_worker(src_imgs: &[RgbImage], time: u32, width: u32, height: u32, frame: u32) -> u32 {
-    //Create the new image buffer with the right size
-    let mut new_img = RgbImage::new(time, height);
+    //When we're working with the images, they're rotated 90 degrees clockwise
+    //so X is from top to bottom, this takes advantage of image byte ordering to
+    //more quickly copy rows
+    let column_pitch = BPP * height;
+    let source_column = ((frame - 1) * column_pitch) as usize..(frame * column_pitch) as usize;
+
+    let mut new_img_buffer: Vec<u8> = vec![0; (column_pitch * time) as usize];
     for x in 0..time {
-        for y in 0..height {
-            new_img.put_pixel(x, y, *src_imgs[x as usize].get_pixel(frame - 1, y));
-        }
+        let destination_column = (x * column_pitch) as usize..((x + 1) * column_pitch) as usize;
+        new_img_buffer[destination_column]
+            .copy_from_slice(&src_imgs[x as usize].as_raw()[source_column.clone()]);
     }
 
-    //Downsize to the proper dimension
+    let mut new_img = RgbImage::from_raw(height, time, new_img_buffer).unwrap();
+
+    //Unrotate and unresize to the proper dimensions
+    new_img = rotate270(&new_img);
     new_img = resize(&new_img, width, height, FilterType::CatmullRom);
 
     //Figure out the proper path
